5a1c45f3f319a90da74c4623748a61712de31de6
Habteyes Asfaw <habteyes.asfaw@hst-et.com>
2024-10-17 13:52:48 +0300

    Update test_preprocessing.py

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import sys
 import os
+import pytest
+
+# Append the scripts directory to the system path
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), '../scripts')))
-from preprocessing import(handle_missing_values, data_cleaning, feature_engineering, ip_to_int, encode_categorical)
+
+# Import from preprocess module
+from preprocessing import handle_missing_values, data_cleaning, feature_engineering, ip_to_int, encode_categorical
+
 import numpy as np
 
 def test_handle_missing_values():
