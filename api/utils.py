f5ea6c8417bd87ff3ab3421dee24fd68fa23065a
Habteyes Asfaw <habteyes.asfaw@hst-et.com>
2024-10-26 23:57:25 +0300

    Implement load_model and initialize_logger functions in utils.py

--- api/utils.py
+++ api/utils.py
@@ -7,7 +7,7 @@ def load_model(model_path):
     return joblib.load(model_path)
 
 def initialize_logger():
-    logger = logging.getLogger('fraud_detection')
+    logger = logging.getLogger('fraud_detections')
     logger.setLevel(logging.INFO)
     handler = logging.FileHandler('../logs/app.log')
     handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s'))
